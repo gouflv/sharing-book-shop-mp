2db6c44a6d59e224ca6409e1e27f981b3263faaa
gouflv <lv.gouf@gmail.com>
2020-07-20 20:06:25 +0800

    doc

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -27,11 +27,11 @@ export default () => {
     // Taro.redirectTo({ url: '/pages/baby-profile/index' })
     // Taro.redirectTo({ url: '/pages/user-order/index' })
     // Taro.redirectTo({ url: '/pages/user-message/index' })
-    // Taro.redirectTo({
-    //   url: '/pages/subject-detail/index?id=202007171018176257'
-    // })
+    Taro.redirectTo({
+      url: '/pages/subject-detail/index?id=202007171018176257'
+    })
 
-    Taro.switchTab({ url: '/pages/home/index' })
+    // Taro.switchTab({ url: '/pages/home/index' })
     // Taro.switchTab({ url: '/pages/user/index' })
     // Taro.switchTab({ url: '/pages/subject/index' })
     // Taro.switchTab({ url: '/pages/order/index' })
